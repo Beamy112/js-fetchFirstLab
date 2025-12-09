ca71bfab9f71a03f250bbabbf4e03e8cd78397f5
Denys Terentiev <140820127+Beamy112@users.noreply.github.com>
2025-12-09 10:09:34 +0200

    Update task3.js

--- tasks/task3.js
+++ tasks/task3.js
@@ -1,14 +1,26 @@
+
 "Ваш код повинен зробити PATCH-запит до вказаного URL, де {userId} – це ID існуючого користувача."
 "Для оновлення користувача передайте в запит нові дані, наприклад, нове ім’я."
 "Поверніть відповідь від сервера з оновленими даними користувача."
 
 "https://jsonplaceholder.typicode.com/users - адреса куди робити запит"
+async function updateUser(id, updatedData) {
+  const url = `https://jsonplaceholder.typicode.com/users/${id}`;
 
+  const response = await fetch(url, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(updatedData)
+  });
 
-function updateUser(id, updatedData) {
-  // Ваш код
+  const data = await response.json();
+
+  return {
+    id: id,           
+    ...data          
+  };
 }
-
-console.log(updateUser(1, { name: 'New Name' }));
 
 module.exports = updateUser;
